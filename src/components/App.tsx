a588f3bb72726699a1760a9aa2ed11deec7caa42
arturjzapater <zapater.artur@gmail.com>
2020-08-10 10:42:50 +0200

    removed unused code

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,12 @@
 import React from 'react'
-import { connect } from 'react-redux'
 import AddMealForm from './AddMealForm'
 import Button from './Button'
 import Header from './Header'
 import MealSchedule from './MealSchedule'
-import ShoppingList from './ShoppingList'
-import actions from '../actions'
-import mapStateToProps from '../lib/mapStateToProps'
 import printPdf from '../lib/printPdf'
 import State from '../types/State'
 
-interface AppProps extends State {
-  addIngredient: CallableFunction,
-}
-
-const App: React.FC<AppProps> = ({
-  ingredients,
-  addIngredient
-}) => (
+const App: React.FC<State> = () => (
   <>
     <Header />
     <main className='mx-4 md:mx-10 lg:mx-16 bg-purple-200 p-2 md:p-6'>
@@ -28,19 +17,8 @@ const App: React.FC<AppProps> = ({
         text='Save as PDF'
         theme='dark'
       />
-      <div>
-        <ShoppingList ingredients={ingredients} />
-        <button onClick={() => addIngredient('Potatoes')}>Add Potatoes</button>
-      </div>
     </main>
   </>
 )
 
-const mapDispatchToProps = (dispatch: CallableFunction) => ({
-  addIngredient: (ingredient: string) => dispatch(actions.addIngredient(ingredient))
-})
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App)
+export default App
